d0e41339371c74ef5c1b3b366a6928731353f1ab
jcmenaartero <jcmenaartero@gmail.com>
2021-02-24 10:59:18 +0100

    Libreria APIrest

---
+++ libreria/apilibreria/src/Config/config.php
@@ -0,0 +1,11 @@
+<?php 
+namespace App\Config;
+
+interface config {
+    CONST DB_NAME    = "libreria";
+    CONST DB_USER    = "root";
+    CONST DB_PORT    = "33060";
+    CONST DB_PASS    = "";
+    CONST DB_CHARSET = "utf8";
+    CONST DB_HOST    = "localhost";
+}
