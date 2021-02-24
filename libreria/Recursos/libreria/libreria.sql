0b817354a7a107b170f59db2682463cfdaaca000
jcmenaartero <jcmenaartero@gmail.com>
2021-02-24 19:23:48 +0100

    libreria.sql modificado

--- libreria/Recursos/libreria/libreria.sql
+++ libreria/Recursos/libreria/libreria.sql
@@ -20,9 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `libreria`
 --
-
+CREATE DATABASE `libreria`;
 -- --------------------------------------------------------
-
+USE `libreria`;
 --
 -- Estructura de tabla para la tabla `categorias`
 --
