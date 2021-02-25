940306759a6127632bde4f5480eb404d8691f453
root <root@vmi519927.contaboserver.net>
2021-02-25 13:54:31 +0100

    Midelware

--- libreria/apilibreria/src/Config/config.php
+++ libreria/apilibreria/src/Config/config.php
@@ -4,8 +4,8 @@ namespace App\Config;
 interface config {
     CONST DB_NAME    = "libreria";
     CONST DB_USER    = "root";
-    CONST DB_PORT    = "33060";
-    CONST DB_PASS    = "";
+    CONST DB_PORT    = "3306";
+    CONST DB_PASS    = "usuario";
     CONST DB_CHARSET = "utf8";
     CONST DB_HOST    = "localhost";
 }
