44d2954751a6ac995fa7b27e08710161c7e8d9dc
jcmenaartero <jcmenaartero@gmail.com>
2021-03-03 12:39:29 +0100

    Comprobando ruta a libros.php v5

--- libreria/apilibreria/src/Routes/libros.php
+++ libreria/apilibreria/src/Routes/libros.php
@@ -1,6 +1,8 @@
 <?php
 # para agrupar las rutas en grupos
 use Slim\Routing\RouteCollectorProxy;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 
 //contendrá los entrypoints (acciones CRUD) de la tabla libros
 /*$app->group('/api', function(RouteCollectorProxy $group){
