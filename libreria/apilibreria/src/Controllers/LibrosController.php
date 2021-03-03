57167ec72a4c97f335e133c54d3e68e6349a5bfc
jcmenaartero <jcmenaartero@gmail.com>
2021-03-03 18:06:00 +0100

    Comprobando acceso a BBDD

--- libreria/apilibreria/src/Controllers/LibrosController.php
+++ libreria/apilibreria/src/Controllers/LibrosController.php
@@ -3,7 +3,7 @@
     use Psr\Http\Message\ResponseInterface as Response;
     use Psr\Http\Message\ServerRequestInterface as Request;
     //use App\Controllers\BaseController;
-    //use App\Model\LibrosModel;    
+    use App\Model\LibrosModel;    
 
     class LibrosController {
         
@@ -14,14 +14,15 @@
                 ->withStatus(200);
         }*/
         public function getAll($request, $response, $args){
-            /*$libros = LibrosModel::getAll();
+            $libros = LibrosModel::getAll();
             $librosJson = json_encode($libros);
             $response->getBody()->write($librosJson);
             return $response
                 ->withHeader('Content-Type', 'application/json')
-                ->withStatus(200);*/
+                ->withStatus(200);
+            /*//FUNCION DE PRUEBA /index.php/api/libros
             $response->getBody()->write("Hello, I'm your LibrosController.php file");
-            return $response;
+            return $response;*/
             
         }
     }
