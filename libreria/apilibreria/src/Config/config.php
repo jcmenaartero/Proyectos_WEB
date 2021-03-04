be601fa13231e7f483e7c1843562b945888bcf69
jcmenaartero <jcmenaartero@gmail.com>
2021-03-04 14:24:01 +0100

    Acceso final BBDD

--- libreria/apilibreria/src/Config/config.php
+++ libreria/apilibreria/src/Config/config.php
@@ -3,7 +3,7 @@ namespace App\Config;
 
 interface config {
     CONST DB_NAME    = "libreria";
-    CONST DB_USER    = "root";
+    CONST DB_USER    = "juank";
     CONST DB_PORT    = "3306";
     CONST DB_PASS    = "usuario";
     CONST DB_CHARSET = "utf8";
